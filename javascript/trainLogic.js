013a084163de44505b5520a979173d9a0f2c23e8
Mai Houa Vue <mhvue4@gmail.com>
2020-01-26 21:39:55 -0600

    updated information of application in ReadMe. commented out btn code due to nonworking for now

--- javascript/trainLogic.js
+++ javascript/trainLogic.js
@@ -12,12 +12,9 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 
   //var to hold our database 
-
   var database = firebase.database();
 
-
   //on click from submit button 
-
   $("#addTrain-btn").on("click", function() {
     event.preventDefault();
     // console.log("cilck");
@@ -37,8 +34,7 @@ var firebaseConfig = {
       frequency: frequency
 };
 
-
-// need to store the intital data from firebase database (the ref() and push () )
+// need to store the intital data from firebase database 
 database.ref().push( trainForm);
 
 //clear form after push
@@ -49,21 +45,16 @@ $("#freq-input").val(" ");
 
 }); //closure for click function 
 
- 
-
   //firebase watcher(snapshot) and snapshot.val() to add a train
   // (which is a child so needed childSnpshot) into our Current Train Schedule 
 database.ref().on("child_added", function(childSnapshot) {
-  console.log(childSnapshot.val());
-
+  
   //variable to hold our child info 
   var trainName = childSnapshot.val().train;
   var destination =childSnapshot.val().destination;
   var firstTrainTime = childSnapshot.val().firstTrainTime;
   var frequency= childSnapshot.val().frequency;
- 
   console.log(trainName, destination, firstTrainTime, frequency);
-
 
 var firstTrainTime=0;
 //moment.js here
@@ -96,23 +87,39 @@ console.log(arrivalData);
 var minAway= moment(arrivalTime).diff(moment(), "minutes");
 console.log("Min away: " + minAway);
 
+//creating the buttons to give user options to remove train info 
+var removeBtn = $("<button>");
+removeBtn.attr("data-train", trainName);
+removeBtn.addClass("deleteTrain");
+removeBtn.text("X");
+  
+
+
 //display on HTML using jquery by creating rows 
-var trainRow= $("<tr>").append(
-  $("<td>").text(trainName), 
-  $("<td>").text(destination),
-  $("<td>").text(frequency), 
-  $("<td>").text(arrivalData), 
-  $("<td>").text(minAway)
-  );
+var trainRow= $("<tr>").attr("id", "trainInfo").append(
+// $("<td>").text(trainName).prepend(removeBtn), commented this out due to unable to add remove button to work
+$("<td>").text(trainName),
+$("<td>").text(destination),
+$("<td>").text(frequency), 
+$("<td>").text(arrivalData), 
+$("<td>").text(minAway)
+);
 
 
+// Attempting to add remove btn...this is the  clicked function but unable to get td to remove 
+// $(document.body).unbind().on("click", ".deleteTrain", function() {
+//   console.log("click");
+//   var testingBtn = $(this).attr("data-train");
+//   console.log(testingBtn);
+//   $(".trainInfo" + testingBtn).remove();
+// });
 
 //append to table  of Curren Train Schedule
 $("#trainSchedule-table").append(trainRow);
-//error handler 
 },function(errorObject) {
   console.log("Errors handled: " + errorObject.code);
 });
+
 
 
 
